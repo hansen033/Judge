deff0a02f11339a8c1aa51440649ff88c736b772
Hansen Chien <32165157+hansen033@users.noreply.github.com>
2023-01-24 13:30:17 +0800

    Update d769. 共有多少種走法.cpp
    undo 逆優化

--- ZeroJudge/d769. 共有多少種走法.cpp
+++ ZeroJudge/d769. 共有多少種走法.cpp
@@ -7,7 +7,7 @@ int main(){
 	while(cin >> n >> k){
 		//create array
 		int ft = log2(k);
-		int matrixes[ft+1][n+1][n+1], ans[2][n+1][n+1];
+		int matrixes[ft+1][n+1][n+1], ans[n+1][n+1], temp[n+1][n+1];
 		fill(&matrixes[0][0][0], &matrixes[ft][n][n]+1, 0);
 		
 		//route input
@@ -23,7 +23,7 @@ int main(){
 			for(int t1 = 1; t1 <= n; t1++){
 				for(int t2 = 1; t2 <= n; t2++){
 					for(int t3 = 1; t3 <= n ; t3++){
-						matrixes[t0][t1][t2] += matrixes[t0-1][t1][t3] * matrixes[t0-1][t3][t2];
+						matrixes[t0][t1][t2] = matrixes[t0][t1][t2] + matrixes[t0-1][t1][t3] * matrixes[t0-1][t3][t2];
 					}
 					matrixes[t0][t1][t2] = matrixes[t0][t1][t2] % 5281;
 				}
@@ -33,21 +33,25 @@ int main(){
 		//ans
 		for(int t1 = 0; t1 <= n; t1++){
 			for(int t2 = 0; t2 <= n; t2++)
-				ans[0][t1][t2] = matrixes[ft][t1][t2];
+				ans[t1][t2] = matrixes[ft][t1][t2];
 		}
-		int left = k - pow(2, ft), cona = 1, conl = 0;
+		int left = k - pow(2, ft);
 		for(int t0 = ft-1; t0 >= 0; t0--){
 			if(left >= pow(2, t0)){
-				fill(&ans[cona][0][0], &ans[cona][n][n]+1, 0);
 				for(int t1 = 1; t1 <= n; t1++){
 					for(int t2 = 1; t2 <= n; t2++){
+						temp[t1][t2] = 0;
 						for(int t3 = 1; t3 <= n ; t3++){
-							ans[cona][t1][t2] += ans[conl][t1][t3] * matrixes[t0][t3][t2];
+							temp[t1][t2] = temp[t1][t2] + ans[t1][t3] * matrixes[t0][t3][t2];
 						}
-						ans[cona][t1][t2] = ans[cona][t1][t2] % 5281;
+						temp[t1][t2] = temp[t1][t2] % 5281;
 					}
 				}
-				swap(cona, conl);
+				for(int t1 = 0; t1 <= n; t1++){
+					for(int t2 = 0; t2 <= n; t2++){
+						ans[t1][t2] = temp[t1][t2];
+					}
+				}
 				left -= pow(2, t0);
 			}
 		}
@@ -57,7 +61,7 @@ int main(){
 		cin >> g;
 		for(int t = 0; t < g; t++){
 			cin >> mb >> nb;
-			cout << ans[conl][mb][nb] << endl;
+			cout << ans[mb][nb] << endl;
 		}
 	}
 }
