f20d39bf78a73fcf81a46c70cd43af470a96685b
Hansen Chien <32165157+hansen033@users.noreply.github.com>
2023-02-20 20:59:35 +0800

    Create 10055 - Hashmat the Brave Warrior.cpp

---
+++ UVA/10055 - Hashmat the Brave Warrior.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	long long Hashmat, opponent;
+	while(cin >> Hashmat){
+		cin >> opponent;
+		cout << abs(opponent - Hashmat) << endl;
+		//or vice versa
+	}
+}
